b086e9a9a281acb4bdc2a31ece8ea8d4c4cceaf3
JB-Goins <goinsjasmine07@gmail.com>
2024-12-13 14:26:19 -0500

    i accomplished getting the new customer info to appear in firebase

--- app/src/main/java/com/example/customerappjg3/AddCustomerActivity.java
+++ app/src/main/java/com/example/customerappjg3/AddCustomerActivity.java
@@ -8,9 +8,16 @@ import android.widget.EditText;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class AddCustomerActivity extends AppCompatActivity {
     Button subBTN;
     EditText fnET, lnET, emailET, phoneET;
+    String  fName,  lName,  email,  phone;
+    Customer myCustomer;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +29,24 @@ public class AddCustomerActivity extends AppCompatActivity {
         lnET = findViewById(R.id.lnET);
         emailET = findViewById(R.id.emailET);
         phoneET= findViewById(R.id.phoneET);
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message");
 
         subBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                fName = fnET.getText().toString();
+                lName = lnET.getText().toString();
+                email = emailET.getText().toString();
+                phone = phoneET.getText().toString();
+                Customer myCustomer = new Customer(fName, lName, email, phone);
+                //myRef.setValue(myCustomer);
+                String key = myRef.push().getKey();
+                myRef.child(key).setValue(myCustomer);
 
             }
         });
+
         
 
     }
